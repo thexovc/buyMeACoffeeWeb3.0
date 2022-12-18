f23c9a854bbfcd8de9d109558f01a2fb0504c836
Osariemen Daniel Osazee <63324071+kyilax@users.noreply.github.com>
2022-12-18 15:23:30 +0100

    done with dashboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <div className='w-full flex md:px-40 justify-center items-center px-2 mt-5'>
+            <div className='md:w-1/3 hidden md:flex'>sjs</div>
+            <div className='md:w-2/3 w-full bg-[#F9F8F8] md:px-4 px-1 flex text-xl flex-col'>
+                <div className="flex border-b-2 p-8">
+                    <div className="image">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="flex flex-col gap-3">
+                        <h1 className='text-xl font-semibold'>Hi, Caleb Peter</h1>
+                        <p className='text-lg'>buymeacoffee.com/calebpeter.c</p>
+                    </div>
+                </div>
+
+                <div className="flex flex-col p-8">
+                    <div className="earn flex">
+                        <h1>Earnings</h1>
+                    </div>
+                    <p className='text-2xl'>0 eth</p>
+                </div>
+
+                <div className='flex justify-center my-10'>
+                    <p className='cursor-pointer bg-[#FF6937] flex justify-center gap-3 items-center w-1/3 text-xl font-semibold text-white p-4 rounded-xl'>
+                        Withdraw</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
 
 
-        <footer class="w-full flex justify-center p-4">
+        <footer class="w-full fixed bottom-0 flex justify-center p-8">
             <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href='https://twitter.com/thexovc' target="_blank" className='underline' class="hover:underline">danielOsariemen.eth</a>. All Rights Reserved.
             </span>
 
